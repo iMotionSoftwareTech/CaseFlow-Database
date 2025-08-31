5cbc456db1aeccf6d192afa016113db60ed33697
Abdul Naqshband <imotionsoftware@hotmail.com>
2025-08-31 22:37:04 +0100

    Closes #4  - Implement Caseworker table and rename caseworker role table and file.

--- CaseFlow-Database/CaseFlow-Database/Tables/caseflow.CaseWorkerRole.sql
+++ CaseFlow-Database/CaseFlow-Database/Tables/caseflow.CaseWorkerRole.sql
@@ -1,10 +1,9 @@
-﻿CREATE TABLE [caseFlow].[CaseWorkerRole]
+﻿CREATE TABLE [caseFlow].[CaseworkerRole]
 (
 	[Id] INT NOT NULL IDENTITY(1,1),
 	[Name] NVARCHAR(256) NOT NULL,
 	[Description] NVARCHAR(500) NULL,
-	CONSTRAINT PK_CaseWorkerRole  PRIMARY KEY ([Id])
+	CONSTRAINT PK_CaseworkerRole PRIMARY KEY ([Id])
 )
 GO 
-
-	CREATE UNIQUE NONCLUSTERED INDEX IX_CaseWorkerRole_Id on caseFlow.CaseWorkerRole([Id])
+	CREATE UNIQUE NONCLUSTERED INDEX IX_CaseworkerRole_Id on [caseFlow].[CaseworkerRole]([Id])
