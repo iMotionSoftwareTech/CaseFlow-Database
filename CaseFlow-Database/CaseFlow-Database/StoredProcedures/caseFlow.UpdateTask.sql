21407d414d68c4209d942c5757be247a8352209e
Abdul Naqshband <imotionsoftware@hotmail.com>
2025-09-04 04:46:33 +0100

    Closes #11 - Implement the update task stored procedure and also add Id column to Task Status table

---
+++ CaseFlow-Database/CaseFlow-Database/StoredProcedures/caseFlow.UpdateTask.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [caseFlow].[UpdateTask]
+	@taskId			INT,
+	@statusId		INT,
+	@caseworkerId	INT,
+	@notes			NVARCHAR(256)
+AS
+BEGIN TRY	
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+
+	IF EXISTS (SELECT 1 FROM [caseFlow].[TaskStatus] WHERE TaskId = @taskId AND StatusId = @statusId)
+		THROW 51002, 'Task has already been updated.', 1;
+	
+	
+	INSERT INTO [caseFlow].[TaskStatus]		(TaskId, StatusId, CaseworkerId, Notes)
+	VALUES									(@taskId, @statusId, @caseworkerId, @notes)
+
+	SET NOCOUNT OFF;
+END TRY
+BEGIN CATCH
+	THROW;
+END CATCH
