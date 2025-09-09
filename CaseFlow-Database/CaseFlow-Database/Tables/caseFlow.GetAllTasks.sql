6ca28ec5a92270bde0f96d135d46a64b5f5ab748
Abdul Naqshband <imotionsoftware@hotmail.com>
2025-09-09 01:50:42 +0100

    Closes #16  - Implement store procedure to get all tasks.

---
+++ CaseFlow-Database/CaseFlow-Database/Tables/caseFlow.GetAllTasks.sql
@@ -0,0 +1,32 @@
+﻿CREATE PROCEDURE [caseFlow].[GetAllTasks]	
+	@pageNumber			INT,
+	@pageSize			INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	-- Return count of records
+	SELECT COUNT(*) AS 'TotalNoOfRecords' FROM [caseFlow].[Task]
+
+	-- If No page size is given return all records
+	IF @pageSize = -1
+	BEGIN 
+		SELECT [Id], [Title], [Description], [DueDateTime]	
+		FROM [caseFlow].[Task]
+		ORDER BY [Id]
+	END
+	ELSE 
+	BEGIN
+		-- Calculate number of rows to skip
+		DECLARE @offset	INT = (@pageNumber - 1) * @pageSize
+
+		-- Return the paginated data of Tasks 
+		SELECT [Id], [Title], [Description], [DueDateTime]	
+		FROM [caseFlow].[Task]
+		ORDER BY [Id]
+		OFFSET @offset ROWS
+		FETCH NEXT @pageSize ROWS ONLY
+	END
+
+	SET NOCOUNT OFF;
+END
